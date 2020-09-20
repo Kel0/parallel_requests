52a3dd940ffd6ea53e3751de453cd9061b5a6d47
Kel0 <rozovdima123@gmail.com>
2020-09-20 11:27:55 +0600

    #9 Add extended session to init and rewrite some doc strings

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -2,8 +2,12 @@
 This module provides only two HTTP request methods benchmarks
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-1. GET request - ~12.961835145950317
-2. POST request - ~10.807614088058472
+1. Parallel GET request - ~12.961835145950317
+2. Parallel POST request - ~10.807614088058472
+
+1. Synchronous GET request - ~41.12413253263
+2. Synchronous POST request - ~40.95021678264
+
 
 Notice that speed of requests depends to your internet connection speed
 
