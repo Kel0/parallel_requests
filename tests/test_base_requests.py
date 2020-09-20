8fc380935c586b604d52afb7faba462b3e578221
Kel0 <rozovdima123@gmail.com>
2020-09-20 10:48:16 +0600

    #1 Add tests

--- tests/test_base_requests.py
+++ tests/test_base_requests.py
@@ -1,25 +1,90 @@
 import pytest
 from requests import Response
 
-from parallel_requests.base_requests import request
-
 from .conftest import FakeFuture
+
+from parallel_requests.base_requests import (  # isort:skip
+    delete,
+    get,
+    head,
+    options,
+    patch,
+    post,
+    put,
+    request,
+)
 
 
 @pytest.fixture
-def mock_parallel_requests_requests_request(mocker, helpers):
-    mock_parallel_requests_requests_request = mocker.patch(
+def mock_parallel_requests_requests_future(mocker, helpers):
+    mock_parallel_requests_requests_future = mocker.patch(
         "parallel_requests.base_requests.futures"
     )
-    mock_parallel_requests_requests_request.executor.return_value.submit = None
-    mock_parallel_requests_requests_request.as_completed.return_value = [FakeFuture]
-    return mock_parallel_requests_requests_request
+    mock_parallel_requests_requests_future.executor.return_value.submit = None
+    mock_parallel_requests_requests_future.as_completed.return_value = [FakeFuture]
+    return mock_parallel_requests_requests_future
 
 
-def test_request(mock_parallel_requests_requests_request, helpers):
+@pytest.mark.parametrize("method", ["post", "get", "head", "options", "put", "patch"])
+def test_request(mock_parallel_requests_requests_future, method, helpers):
     responses = request(
-        method_args=[{"url": "https://www.python.org", "method": "post"}],
+        method_args=[{"url": "https://www.python.org", "method": method}],
     )
 
-    assert isinstance(responses[0], Response)
-    assert isinstance(helpers().response_object_200_msg(), Response)
+    assert all(response for response in responses if isinstance(response, Response))
+
+
+def test_post(mock_parallel_requests_requests_future, helpers):
+    responses = post(
+        method_args=[{"url": "https://www.python.org"}],
+    )
+
+    assert all(response for response in responses if isinstance(response, Response))
+
+
+def test_get(mock_parallel_requests_requests_future, helpers):
+    responses = get(
+        method_args=[{"url": "https://www.python.org"}],
+    )
+
+    assert all(response for response in responses if isinstance(response, Response))
+
+
+def test_options(mock_parallel_requests_requests_future, helpers):
+    responses = options(
+        method_args=[{"url": "https://www.python.org"}],
+    )
+
+    assert all(response for response in responses if isinstance(response, Response))
+
+
+def test_put(mock_parallel_requests_requests_future, helpers):
+    responses = put(
+        method_args=[{"url": "https://www.python.org"}],
+    )
+
+    assert all(response for response in responses if isinstance(response, Response))
+
+
+def test_patch(mock_parallel_requests_requests_future, helpers):
+    responses = patch(
+        method_args=[{"url": "https://www.python.org"}],
+    )
+
+    assert all(response for response in responses if isinstance(response, Response))
+
+
+def test_delete(mock_parallel_requests_requests_future, helpers):
+    responses = delete(
+        method_args=[{"url": "https://www.python.org"}],
+    )
+
+    assert all(response for response in responses if isinstance(response, Response))
+
+
+def test_head(mock_parallel_requests_requests_future, helpers):
+    responses = head(
+        method_args=[{"url": "https://www.python.org"}],
+    )
+
+    assert all(response for response in responses if isinstance(response, Response))
