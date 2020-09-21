98724019bdf9062fbf67ae9ceec1ffd8d66ec9ff
Kel0 <rozovdima123@gmail.com>
2020-09-22 01:04:15 +0600

    #13 Add code examples

--- parallel_requests/__init__.py
+++ parallel_requests/__init__.py
@@ -1,2 +1,2 @@
 from .base_requests import ListResponse, get, post, request  # noqa: F841,F401
-from .session import Session  # noqa: F841,F401
+from .session import Session, extended_session  # noqa: F841,F401
